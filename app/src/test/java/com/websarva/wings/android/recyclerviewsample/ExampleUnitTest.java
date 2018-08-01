5d4ea5fde935be146be557609e98cf502a70975f
disk3457 <dsakkyan0304@yahoo.co.jp>
2018-08-01 10:22:12 +0900

    Initial commit

---
+++ app/src/test/java/com/websarva/wings/android/recyclerviewsample/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package com.websarva.wings.android.recyclerviewsample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+	@Test
+	public void addition_isCorrect() throws Exception {
+		assertEquals(4, 2 + 2);
+	}
+}
